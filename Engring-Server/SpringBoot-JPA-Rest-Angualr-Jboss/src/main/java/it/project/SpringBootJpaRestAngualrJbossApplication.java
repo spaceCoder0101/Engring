965b80509010a5e7bda61fbe450f4d6d6dcf3d14
davidacab <babadragosdavid27091999@gmail.com>
2022-10-28 17:08:35 +0200

    First Commit - Empty Project

---
+++ Engring-Server/SpringBoot-JPA-Rest-Angualr-Jboss/src/main/java/it/project/SpringBootJpaRestAngualrJbossApplication.java
@@ -0,0 +1,13 @@
+package it.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaRestAngualrJbossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaRestAngualrJbossApplication.class, args);
+	}
+
+}
