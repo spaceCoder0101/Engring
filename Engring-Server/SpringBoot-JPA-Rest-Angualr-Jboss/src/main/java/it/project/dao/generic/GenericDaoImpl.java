f44452d90803a03b1549897f62a1e23ff3fda2de
davidacab <babadragosdavid27091999@gmail.com>
2022-10-28 19:07:14 +0200

    #6 Modify GenericDaoImpl -Add transaction

--- Engring-Server/SpringBoot-JPA-Rest-Angualr-Jboss/src/main/java/it/project/dao/generic/GenericDaoImpl.java
+++ Engring-Server/SpringBoot-JPA-Rest-Angualr-Jboss/src/main/java/it/project/dao/generic/GenericDaoImpl.java
@@ -2,6 +2,7 @@ package it.project.dao.generic;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 public abstract class GenericDaoImpl<T> implements GenericDao<T> {
 
@@ -9,16 +10,19 @@ public abstract class GenericDaoImpl<T> implements GenericDao<T> {
 	protected EntityManager em;
 	
 	@Override
+	@Transactional
 	public void persist(T obj) {
 		em.persist(obj);	
 	}
 	
 	@Override
+	@Transactional
 	public void remove(T obj) {
 		em.remove(em.contains(obj)?obj:em.merge(obj));
 	}
 	
 	@Override
+	@Transactional
 	public void merge(T obj) {
 		em.merge(obj);	
 	}
