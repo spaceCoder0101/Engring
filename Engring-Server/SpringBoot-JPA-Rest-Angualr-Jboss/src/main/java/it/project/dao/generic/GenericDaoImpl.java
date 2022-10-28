5b4e894cd0d418e43c53c44992eea6bd5e679ad7
davidacab <babadragosdavid27091999@gmail.com>
2022-10-28 18:16:48 +0200

    #3 Add abstraction to dao layer

---
+++ Engring-Server/SpringBoot-JPA-Rest-Angualr-Jboss/src/main/java/it/project/dao/generic/GenericDaoImpl.java
@@ -0,0 +1,25 @@
+package it.project.dao.generic;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+public abstract class GenericDaoImpl<T> implements GenericDao<T> {
+
+	@PersistenceContext
+	EntityManager em;
+	
+	@Override
+	public void persist(T obj) {
+		em.persist(obj);	
+	}
+	
+	@Override
+	public void remove(T obj) {
+		em.remove(em.contains(obj)?obj:em.merge(obj));
+	}
+	
+	@Override
+	public void merge(T obj) {
+		em.merge(obj);	
+	}
+}
